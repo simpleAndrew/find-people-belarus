c86234b322801badc3bf5de43921c4a0545b9e82
Andrew Shchyolok <andrew.shchyolok@strevus.com>
2020-08-14 12:22:20 +0200

    test application of google api

--- js/demo.js
+++ js/demo.js
@@ -3,14 +3,13 @@ var key = "AIzaSyCsPypkGNitFV5SVrbMt3ET3cMg51h-uHw"
 var sheetId = "1NhNyoNQRHrg0Ce-NrZle6NeWCIxGa1L07WBMgOisnIM"
 
 $().ready(function() {
-    $("#text").html(fetchName(sheetId));
+    fetchName(sheetId)
 });
 
 var fetchName = function(sheetId){
     var url = 'https://spreadsheets.google.com/feeds/list/'+sheetId+'/values/Окрестино!A1:B2?key='+key;
-
-    $.get( url, function( data ) {
-        //Parse and Do Your Stuff Here
+    $.get( url, function( data , tS, jq) {
+        $("#text").html(data);
     });
 }
 
