ccf6bc225f8a994efc6f1236e54de13fe2cdd95c
Andrew Shchyolok <andrew.shchyolok@strevus.com>
2020-08-14 17:27:20 +0200

    add form

--- js/demo.js
+++ js/demo.js
@@ -4,30 +4,31 @@ var sheetId = "1NhNyoNQRHrg0Ce-NrZle6NeWCIxGa1L07WBMgOisnIM"
 $(function () {
     $("#search_submit").click(function (e) {
         e.preventDefault();
-        let filterName = "Але"
+        let filterName = $("#surname").innerText
+        alert("Search for surname: " + filterName)
         searchForPeople(sheetId, "Окрестино", filterName)
     });
 });
 
 
 function searchForPeople(sheetId, tabName, filterWord) {
-    $.get( sheetUrl(sheetId, tabName), function( data ) {
+    $.get(sheetUrl(sheetId, tabName), function (data) {
         let objs = data.values
             .map(jsonRec => convertIntoObj(jsonRec))
             .filter(r => r.fullName != null)
             .filter(r => r.fullName.includes(filterWord))
-            .map( r => r.toHtml())
+            .map(r => r.toHtml())
             .join("")
-        alert(objs)
         $("#result").html("<div>" + objs + "</div>");
     });
 }
+
 function sheetUrl(sheetId, tabName) {
     return 'https://sheets.googleapis.com/v4/spreadsheets/' + sheetId + '/values/' + tabName + '!B2:E1000?key=' + key;
 }
 
 function convertIntoObj(json) {
-    return new Record(json[0],json[1],json[2],json[3])
+    return new Record(json[0], json[1], json[2], json[3])
 }
 
 class Record {
@@ -37,10 +38,11 @@ class Record {
         this.location = location
         this.date = date
     }
+
     toHtml() {
         return "<div>" + this.fullName + "</div>"
-        + "<div>" + this.year + "</div>"
-        + "<div>" + this.location + "</div>"
-        + "<div>" + this.date + "</div>"
+            + "<div>" + this.year + "</div>"
+            + "<div>" + this.location + "</div>"
+            + "<div>" + this.date + "</div>"
     }
 }
