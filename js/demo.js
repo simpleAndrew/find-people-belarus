38d727f4901675ecf69098e8ec9645d81898c1fe
Andrew Shchyolok <andrew.shchyolok@strevus.com>
2020-08-14 13:02:29 +0200

    add form

--- js/demo.js
+++ js/demo.js
@@ -1,23 +1,20 @@
 var key = "AIzaSyCsPypkGNitFV5SVrbMt3ET3cMg51h-uHw"
-
 var sheetId = "1NhNyoNQRHrg0Ce-NrZle6NeWCIxGa1L07WBMgOisnIM"
 
-$().ready(function() {
-    fetchName(sheetId)
-});
+$("#target").submit(
+    function (event) {
+        alert("Handler for .submit() called.");
+        event.preventDefault();
+        fetchName(sheetId)
+    }
+);
 
-$("#target").submit(function( event ) {
-    alert( "Handler for .submit() called." );
-    event.preventDefault();
-    fetchName(sheetId)
-});
-
-var fetchName = function(sheetId){
-    $.get(sheetUrl(sheetId), function( data , tS, jq) {
+var fetchName = function (sheetId) {
+    $.get(sheetUrl(sheetId), function (data, tS, jq) {
         $("#other").html(data);
     });
 }
 
-var sheetUrl= function(sheetId) {
-    'https://sheets.googleapis.com/v4/spreadsheets/'+sheetId+'/values/Окрестино!A1:B2?key='+key;
+var sheetUrl = function (sheetId) {
+    'https://sheets.googleapis.com/v4/spreadsheets/' + sheetId + '/values/Окрестино!A1:B2?key=' + key;
 }
